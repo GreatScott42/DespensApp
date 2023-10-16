70057371ec4d650ba4af8065538efa12687d602b
Fabian <fabian.sepulveda345@gmail.com>
2023-10-16 17:29:25 -0300

    presupuesto added

--- app/src/main/java/me/gs42/despensapp/AcomprarActivity.kt
+++ app/src/main/java/me/gs42/despensapp/AcomprarActivity.kt
@@ -1,8 +1,11 @@
 package me.gs42.despensapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.EditText
 import android.widget.ListView
 import android.widget.TextView
 
@@ -10,6 +13,7 @@ class AcomprarActivity : AppCompatActivity() {
 
     private lateinit var listComprar: ListView
     private lateinit var total: TextView
+    private lateinit var presupuesto: EditText
     private lateinit var products: MutableList<ProductB>
     private lateinit var adapter : ArrayAdapter<ProductB>
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,6 +22,7 @@ class AcomprarActivity : AppCompatActivity() {
 
         listComprar = findViewById(R.id.listaComprar)
         total = findViewById(R.id.total)
+        presupuesto=findViewById(R.id.presupuesto)
 
         // Create a sample list of patients (you should replace this with your data source)
         products = mutableListOf(
@@ -33,7 +38,21 @@ class AcomprarActivity : AppCompatActivity() {
             totalvalor+=(ProductB.cantidad*ProductB.precio)
         }
         total.text = "total:"+totalvalor.toString()
-
+        var presupuestoString = presupuesto.text.toString()
+        var presupuestoValue = Integer.parseInt(presupuestoString)
+        if(presupuestoValue<totalvalor){
+            total.text="te pasaste de tu presupuesto por"+(totalvalor-presupuestoValue)+":("
+        }
+        val updatebutton = findViewById<Button>(R.id.UpdatePre)
+        updatebutton.setOnClickListener {
+            presupuestoString = presupuesto.text.toString()
+            presupuestoValue = Integer.parseInt(presupuestoString)
+            if(presupuestoValue<totalvalor){
+                total.text="te pasaste de tu presupuesto por"+(totalvalor-presupuestoValue)+":("
+            }else{
+                total.text = "total:"+totalvalor.toString()
+            }
+        }
 
         // Create an ArrayAdapter to populate the ListView
 //        adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, patients.map { it.email })
@@ -41,4 +60,5 @@ class AcomprarActivity : AppCompatActivity() {
 
         listComprar.adapter = adapter
     }
+
 }
