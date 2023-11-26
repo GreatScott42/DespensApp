ed039ed54ff3fb5ae721f94b23ef71b2ea087077
Fabian <fabian.sepulveda345@gmail.com>
2023-11-25 22:53:23 -0300

    Database

---
+++ app/src/main/java/me/gs42/despensapp/ProductoDao.kt
@@ -0,0 +1,25 @@
+package me.gs42.despensapp
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+
+@Dao
+interface ProductoDao {
+    @Query("SELECT * FROM producto")
+    fun getAll(): List<Producto>
+
+    @Query("SELECT * FROM producto WHERE uid IN (:userIds)")
+    fun loadAllByIds(userIds: IntArray): List<Producto>
+
+    @Query("SELECT * FROM producto WHERE Nombre LIKE :first AND " +
+            "Precio LIKE :last LIMIT 1")
+    fun findByName(first: String, last: String): Producto
+
+    @Insert
+    fun insertAll(vararg users: Producto)
+
+    @Delete
+    fun delete(user: Producto)
+}
